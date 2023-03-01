810ece9b61ebf42e8938a2bd8b29ab2ad5656176
github_username <you@example.com>
2023-03-01 18:54:33 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,28 @@
+//list insert
+mylist.addEventListener("mousemove", (eo) => {
+  myMenu.style.display="block"
+  myMenu.style.transition=" all 1s"
+
+})
+
+mylist.addEventListener("mouseout", (eo) => {
+   myMenu.style.display="none"
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
